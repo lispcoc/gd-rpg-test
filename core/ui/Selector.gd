d53849ee4d4237b6caebc150cc6665fc45395a73
LISPCoC <fourier0520@gmail.com>
2021-11-25 12:12:54 +0900

    update

--- core/ui/Selector.gd
+++ core/ui/Selector.gd
@@ -3,16 +3,36 @@ extends ItemList
 
 
 export(Array, String) var selection
-export(Array, NodePath) var node
+export(NodePath) var child
 
-func _init(_selection = ["None"]):
+var child_node : ItemList
+
+signal item_nested(array)
+
+func _init(_selection = ["None"], _child : NodePath = ""):
 	selection = _selection
+	child = _child
 
 func _ready():
 	#grab_focus()
 	for n in selection:
 		add_item(n)
-	pass
+
+	var ret 
+	var ret_array = []
+	child_node = get_node_or_null(child)
+	if child_node:
+		add_child(child_node)
+		while ret_array == []:
+			ret = yield(self, "item_activated")
+			child_node.grab_focus()
+			ret_array = yield(child_node, "item_nested")
+	else:
+		ret = yield(self, "item_activated")
+	ret_array.append(ret)
+	print(ret_array)
+	emit_signal("item_nested", ret_array)
+	queue_free()
 
 
 func _process(delta):
